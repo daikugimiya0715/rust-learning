ac18702c3f7ba7118b040f675572175a15c1daf2
daikugimiya0715 <daikugimiya0715@gmail.com>
2026-03-01 18:05:31 +0900

    feat: thread

---
+++ sandbox/thread/src/main.rs
@@ -0,0 +1,28 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!! baby :) ", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // try move :) <3
+    //
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle2.join().unwrap();
+
+    handle.join().unwrap();
+}
